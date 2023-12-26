32025954f9c73858f11210e2e93210fb2b96b107
Filip Filchev <fkfilchev@gmail.com>
2023-12-26 14:15:15 +0200

    Add some Migrations

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Task, Type, Status, Resolution
+
+
+class TaskAdmin(admin.ModelAdmin):
+    model = Task
+    list_display = ('title', 'type', 'description', 'status', 'assignee', 'creator', 'modified', 'resolution', 'parent')
+    list_filter = ('type', 'status', 'assignee', 'creator', 'modified', 'resolution', 'parent')
+    search_fields = ('title', 'type', 'description', 'assignee', 'creator', 'modified', 'resolution', 'parent')
+
+
+class StatusAdmin(admin.ModelAdmin):
+    model = Status
+    list_display = ('status',)
+    search_fields = ('status',)
+
+
+class TypeAdmin(admin.ModelAdmin):
+    model = Type
+    list_display = ('type',)
+    search_fields = ('type',)
+
+
+class ResolutionAdmin(admin.ModelAdmin):
+    model = Resolution
+    list_display = ('resolution',)
+    search_fields = ('resolution',)
+
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Status, StatusAdmin)
+admin.site.register(Type, TypeAdmin)
+admin.site.register(Resolution, ResolutionAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    type = models.ForeignKey('Type', on_delete=models.CASCADE)
+    description = models.TextField(blank=True, null=True)
+    status = models.ForeignKey('Status', on_delete=models.CASCADE)
+    resolution = models.ForeignKey('Resolution', on_delete=models.CASCADE, blank=True, null=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
+    assignee = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='assignee', blank=True, null=True)
+    creator = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='reporter')
+    modified = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Status(models.Model):
+    status = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.status
+
+
+class Type(models.Model):
+    type = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.type
+
+
+class Resolution(models.Model):
+    resolution = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.resolution
